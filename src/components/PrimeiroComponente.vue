ebf5692c13882fdba268deea8319668c7aafe1d0
Edivansanto5 <edivansantos1478@gmail.com>
2022-06-17 21:45:21 -0300

    replicando o mesmo componente em outros

--- src/components/PrimeiroComponente.vue
+++ src/components/PrimeiroComponente.vue
@@ -2,19 +2,23 @@
     <div>
          <h1>Olá vue</h1>
          <p>Meu nome e {{nome}} e tenho {{idade}}</p>
+         <LifecyclehooksVue/>
          
     </div>
    
 </template>
 
 <script>
+import LifecyclehooksVue from './Lifecyclehooks.vue'
+
 export default {
-    name: 'PrimeiroComponente',
-    data(){
-        return{
-           nome:'Edivan dos santos',
-           idade:28
-        }
-    }
+    name: "PrimeiroComponente",
+    data() {
+        return {
+            nome: "Edivan dos santos",
+            idade: 28
+        };
+    },
+    components: { LifecyclehooksVue }
 }
 </script>
