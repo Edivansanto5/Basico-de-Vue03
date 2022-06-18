4bbdccfa2937d7db423d61461bc34dfa68afa695
Edivansanto5 <edivansantos1478@gmail.com>
2022-06-18 20:54:26 -0300

    importando o componente de pessoas em APP

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,22 @@
   <div>
     <PrimeiroComponenteVue />
     <Lifecyclehooks/>
+    <Pessoa/>
+
   </div>
 </template>
 
 <script>
 import PrimeiroComponenteVue from './components/PrimeiroComponente.vue';
 import Lifecyclehooks from './components/Lifecyclehooks.vue';
+import Pessoa from './components/Pessoa.vue'
+
 export default {
     name: "App",
     components: { 
       PrimeiroComponenteVue,
-      Lifecyclehooks 
+      Lifecyclehooks,
+      Pessoa
     }
 }
 </script>
