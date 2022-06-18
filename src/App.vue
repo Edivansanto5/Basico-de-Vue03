eafe1d8d890a41c4e81c2b24b3a3a456ec06817d
Edivansanto5 <edivansantos1478@gmail.com>
2022-06-17 21:11:37 -0300

    importando o componente Lifecycle hooks  para o APP

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <div>
     <PrimeiroComponenteVue />
+    <Lifecyclehooks/>
   </div>
 </template>
 
 <script>
 import PrimeiroComponenteVue from './components/PrimeiroComponente.vue';
+import Lifecyclehooks from './components/Lifecyclehooks.vue';
 export default {
     name: "App",
     components: { 
-      PrimeiroComponenteVue 
+      PrimeiroComponenteVue,
+      Lifecyclehooks 
     }
 }
 </script>
