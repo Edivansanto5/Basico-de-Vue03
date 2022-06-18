f1c69536bcaa99540e7760f49ff6a87081d05a79
Edivansanto5 <edivansantos1478@gmail.com>
2022-06-17 21:09:14 -0300

    criando a estrutura do componente

---
+++ src/components/Lifecyclehooks.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>Meu nome e {{nome}}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'Lifecyclhooks',
+    data(){
+        return{
+            nome: 'Edivan santos'
+        }
+    }
+}
+</script>
