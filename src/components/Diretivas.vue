9bc16d2139f1539ea429400d1b22bf1c77f300b9
Edivansanto5 <edivansantos1478@gmail.com>
2022-06-19 15:57:43 -0300

    Diretiva v-else ativada

--- src/components/Diretivas.vue
+++ src/components/Diretivas.vue
@@ -2,7 +2,7 @@
   <h3>Meu nome é {{nome}}</h3>
   <p>Meus sonho e ser um bom Progamador</p>
   <p v-if="mostrar">Para eu poder ganhar um bom dinheiro e ajudar a minha familia</p>
-  <p>Em caso de desistencia eu serei um mero fracassado da populaçao brasileira</p>
+  <p v-else >Em caso de desistencia eu serei um mero fracassado da populaçao brasileira</p>
 </template>
 
 <script>
@@ -11,7 +11,7 @@
         data(){
             return{
                 nome:'Edivan santos ',
-                mostrar:false,
+                mostrar:true,
             }
         }
     }
