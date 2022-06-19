df5c8f5e9807f6b3623615cce0f85266e0884810
Edivansanto5 <edivansantos1478@gmail.com>
2022-06-19 15:43:49 -0300

    inserindo dados no componente da Diretivas

--- src/components/Diretivas.vue
+++ src/components/Diretivas.vue
@@ -1,10 +1,15 @@
 <template>
-  
+  <p>Meu nome é {{nome}}</p>
 </template>
 
 <script>
     export default {
-
+        name:'Diretivas',
+        data(){
+            return{
+                nome:'Edivan santos '
+            }
+        }
     }
 </script>
 
