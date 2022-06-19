17f849aee545e31d5b58150d52a4176531acf250
Edivansanto5 <edivansantos1478@gmail.com>
2022-06-18 21:57:05 -0300

    importandos os componentes de inputText e Submit dentro do componente form

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -2,15 +2,20 @@
     <form action="">
        
     </form>
+    <InputText/>
+    <Submit/>
 </template>
 
 <script>
+import InputText from './form/InputText.vue'
+import Submit from './form/InputText.vue'
+
 export default {
     name:'Form',
-    data(){
-        return{
+   components:{
+    InputText,
+    Submit
 
-        }
-    }
+   }
 }
 </script>
