71782ffb9e3739cae55ad2c69a6fc72e40035626
Edivansanto5 <edivansantos1478@gmail.com>
2022-06-18 21:17:24 -0300

    importando o componente de Info lá em componente Pessoa

--- src/components/Info.vue
+++ src/components/Info.vue
@@ -1,4 +1,21 @@
 <template>
+  <ul>
+          <li>RUBI</li>
+          <p>PHP</p>
+          <p>PYTHON</p>
+          <p>HTML5</p>
+          <P>CSS5</P>
+          <P>JAVACRIPT</P>
+      </ul>
+      <P>Uso Alguns FrameWork:</P>
+      <ul>
+          <li>React Native</li>
+          <li>React</li>
+          <li>Vue02</li>
+          <li>Vue03</li>
+          <li>node js</li>
+          <li>Flant</li>
+      </ul>
    
 </template>
 
@@ -7,7 +24,7 @@ export default {
     name:'Info',
     data(){
         return{
-            
+
         }
     }
 }
--- src/components/Pessoa.vue
+++ src/components/Pessoa.vue
@@ -3,33 +3,22 @@
       <h2>Essa é uma Descrição da Pessoa chamada {{nome}}</h2>
       <p>Estou trabalhando no momento</p>
       <p>Sou fruente nas Seguintes tecnologias</p>
-      <ul>
-          <li>RUBI</li>
-          <p>PHP</p>
-          <p>PYTHON</p>
-          <p>HTML5</p>
-          <P>CSS5</P>
-          <P>JAVACRIPT</P>
-      </ul>
-      <P>Uso Alguns FrameWork:</P>
-      <ul>
-          <li>React Native</li>
-          <li>React</li>
-          <li>Vue02</li>
-          <li>Vue03</li>
-          <li>node js</li>
-          <li>Flant</li>
-      </ul>
+      <Info/>
+    
     </div>
 </template>
 
 <script>
+import Info from './Info.vue'
 export default {
-    name:'Pessoa',
-    data(){
-        return{
-           nome:'Mario de Sousa Junior'
-        }
+    name: "Pessoa",
+    data() {
+        return {
+            nome: "Mario de Sousa Junior"
+        };
+    },
+    components: { 
+        Info 
     }
 }
 </script>
