4b2de74d6af516cbfcd190be29b8ec2c0a4e0a9b
Edivansanto5 <edivansantos1478@gmail.com>
2022-06-18 21:07:34 -0300

    adicionando dados em componente de pessoas

--- src/components/Pessoa.vue
+++ src/components/Pessoa.vue
@@ -1,6 +1,25 @@
 <template>
     <div>
       <h2>Essa é uma Descrição da Pessoa chamada {{nome}}</h2>
+      <p>Estou trabalhando no momento</p>
+      <p>Sou fruente nas Seguintes tecnologias</p>
+      <ul>
+          <li>RUBI</li>
+          <p>PHP</p>
+          <p>PYTHON</p>
+          <p>HTML5</p>
+          <P>CSS5</P>
+          <P>JAVACRIPT</P>
+      </ul>
+      <P>Uso Alguns FrameWork:</P>
+      <ul>
+          <li>React Native</li>
+          <li>React</li>
+          <li>Vue02</li>
+          <li>Vue03</li>
+          <li>node js</li>
+          <li>Flant</li>
+      </ul>
     </div>
 </template>
 
