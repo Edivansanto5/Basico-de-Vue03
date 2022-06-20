9f60f4e965dd23730307ce8bc5a7ffbd9cacec23
Edivansanto5 <edivansantos1478@gmail.com>
2022-06-20 20:55:36 -0300

    Criando a estrtura basica de um componente

---
+++ src/components/Picture.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+
+    </div>
+</template>
+<script>
+export default{
+    name:'Picture',
+    data(){
+        return{
+            
+        }
+        
+    }
+}
+</script>
